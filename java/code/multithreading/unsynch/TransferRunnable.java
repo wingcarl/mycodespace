f9804970d9f0289f0b4bfde1246a3bdd6c36503f
wingcarl <wingcarlos5@gmail.com>
2015-10-25 22:39:37 +0800

    multithreading unsynch example

---
+++ java/code/multithreading/unsynch/TransferRunnable.java
@@ -0,0 +1,28 @@
+package unsynch;
+
+public class TransferRunnable implements Runnable {
+
+	private Bank bank;
+	private int fromAccount;
+	private double maxAmount;
+	private int DELAY = 10;
+	public TransferRunnable(Bank bank,int from,double max){
+		this.bank = bank;
+		this.fromAccount = from;
+		this.maxAmount = max;
+	}
+	@Override
+	public void run() {
+		try{
+			while(true){
+				int toAccount = (int) (bank.size()*Math.random());
+				double amount = maxAmount * Math.random();
+				bank.transfer(fromAccount, toAccount, amount);
+				Thread.sleep((int)(DELAY*Math.random()));
+			}
+		}catch(InterruptedException e){
+			
+		}
+	}
+
+}
